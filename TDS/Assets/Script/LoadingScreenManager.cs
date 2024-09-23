b31df8cb8e39094cc024a71003500bbb4cc4a6e6
dede2008w <tomypeekblinders@gmail.com>
2024-09-23 14:15:01 -0300

    Revert "Load Scene"
    This reverts commit f487c708da6203cbd890c6700572ff0a76cbd350.

--- TDS/Assets/Script/LoadingScreenManager.cs
+++ TDS/Assets/Script/LoadingScreenManager.cs
@@ -1,11 +1,34 @@
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class LoadingScreenManager : MonoBehaviour
 {
-    // Método que será chamado quando o botão for clicado
-    public void LoadScene()
+    public GameObject loadingCanvas; // Referência ao Canvas da tela de carregamento
+
+    public  void Start()
     {
-        SceneManager.LoadScene("SampleScene");
+        // Inicialmente, o Canvas de carregamento deve estar ativo
+        loadingCanvas.SetActive(true);
+        // Começa a carregar a cena em background
+        StartCoroutine(LoadYourScene());
+    }
+
+    IEnumerator LoadYourScene()
+    {
+        // Substitua "YourSceneName" pelo nome da cena que você deseja carregar
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Telascarregamento");
+
+        // Enquanto a cena está sendo carregada, mantenha a tela de carregamento ativa
+        while (!asyncLoad.isDone)
+        {
+            // Você pode atualizar um progresso ou animação de carregamento aqui se desejar
+            yield return null;
+        }
+
+        // Depois que a cena estiver carregada, desative o Canvas de carregamento
+        loadingCanvas.SetActive(false);
     }
 }
